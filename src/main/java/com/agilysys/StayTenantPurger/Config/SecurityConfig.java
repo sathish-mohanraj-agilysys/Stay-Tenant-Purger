5a72e5d4ea4c1dbe8e65845df3cb0e4d728fc177
Sathish <sathish.m@agilysys.com>
2024-06-21 13:06:38 +0530

    added the spring security

--- src/main/java/com/agilysys/StayTenantPurger/Config/SecurityConfig.java
+++ src/main/java/com/agilysys/StayTenantPurger/Config/SecurityConfig.java
@@ -1,24 +1,22 @@
-//package com.agilysys.StayTenantPurger.Config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/delete/**").permitAll()
-//                .antMatchers("/environment/**").permitAll()
-//                .antMatchers("/*").permitAll()
-//                .anyRequest().authenticated();
-//    }
-//
-//
-//}
-//
+package com.agilysys.StayTenantPurger.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/delete/**").authenticated()
+                .antMatchers("/dropCollections/**").authenticated()
+                .anyRequest().permitAll().and()
+                .httpBasic();
+    }
+}
+
