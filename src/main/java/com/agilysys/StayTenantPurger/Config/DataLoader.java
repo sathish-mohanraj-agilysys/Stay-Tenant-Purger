7e605cc777070db9dc0029f08eae7d89b41581f7
Sathish <sathish.m@agilysys.com>
2024-06-21 17:47:18 +0530

    added the data loader changes

--- src/main/java/com/agilysys/StayTenantPurger/Config/DataLoader.java
+++ src/main/java/com/agilysys/StayTenantPurger/Config/DataLoader.java
@@ -1,24 +1,44 @@
 package com.agilysys.StayTenantPurger.Config;
 
+import com.agilysys.StayTenantPurger.DAO.Tenant;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Map;
 
 @Component
 public class DataLoader {
+    @Autowired
+    ObjectMapper  objectMapper;
+    Yaml yaml = new Yaml();
     public Path RESOURCE_PATH = Paths.get(System.getProperty("user.dir"), "src", "main", "resources");
     public Path BACKUP_PATH = RESOURCE_PATH.resolve("Backup");
+    public String YAML_NAME="rGuestStaymap.yml";
 
-    public File loadYMlFile() {
-        return RESOURCE_PATH.resolve("rGuestStaymap.yml").toFile();
-
+    public synchronized File loadYMlFile() {
+        return RESOURCE_PATH.resolve(YAML_NAME).toFile();
     }
 
     public synchronized File loadCacheFile(String env) {
-
         return RESOURCE_PATH.resolve(env + ".json").toFile();
-
     }
+    public synchronized Tenant readDataFromCacheFile(String env) throws IOException {
+         return objectMapper.readValue(RESOURCE_PATH.resolve(env + ".json").toFile(), Tenant.class);
+    }
+    public synchronized void writeDataIntoCacheFile(String env,Tenant tenant) throws IOException {
+         objectMapper.writeValue(RESOURCE_PATH.resolve(env + ".json").toFile(), tenant);
+    }
+    public synchronized  Map<String, Map<String, ArrayList<String>>> readYMlFile() throws FileNotFoundException {
+       return yaml.load(new FileInputStream(this.loadYMlFile()));
+    }
+
 }
--- src/main/java/com/agilysys/StayTenantPurger/Service/StayDeleteService.java
+++ src/main/java/com/agilysys/StayTenantPurger/Service/StayDeleteService.java
@@ -10,9 +10,7 @@ import org.bson.BsonBinaryWriter;
 import org.bson.BsonWriter;
 import org.bson.Document;
 import org.bson.codecs.DocumentCodec;
-import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.io.BasicOutputBuffer;
-import org.bson.io.OutputBuffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,9 +47,12 @@ public class StayDeleteService {
     private CoreDeleteSerive coreDeleteSerive;
     @Autowired
     private DataLoader dataLoader;
-
+    private Map<String, Map<String, ArrayList<String>>> yamlMap = dataLoader.readYMlFile();
 
     private static final Logger logger = LoggerFactory.getLogger(StayDeleteService.class);
+
+    public StayDeleteService() throws FileNotFoundException {
+    }
 
 
     public String storData(Tenant tenant, String env) {
@@ -76,7 +77,7 @@ public class StayDeleteService {
         Yaml yaml = new Yaml();
         Tenant tenantTemp = null;
         try {
-            File resourceFile =dataLoader.loadCacheFile(env);
+            File resourceFile = dataLoader.loadCacheFile(env);
             tenantTemp = objectMapper.readValue(new FileInputStream(resourceFile), Tenant.class);
         } catch (Exception e) {
             System.out.println("cannot initiate the delete operation");
@@ -138,7 +139,7 @@ public class StayDeleteService {
             return new ResponseEntity<>("Cannot not open the file", HttpStatus.FORBIDDEN);
         }
         try {
-            File resourceFile =dataLoader.loadCacheFile(env);
+            File resourceFile = dataLoader.loadCacheFile(env);
             File backUpFile = Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "Backup", env + ".json").toFile();
             Tenant resourceTenant = objectMapper.readValue(new FileInputStream(resourceFile), Tenant.class);
             Tenant backupTenant = objectMapper.readValue(new FileInputStream(backUpFile), Tenant.class);
@@ -178,69 +179,60 @@ public class StayDeleteService {
     public String getDocumentCountFromCacheDetails(String env) {
         Map<String, Integer> documentCount = new HashMap<>();
         MongoTemplate mongoTemplate = mongoTemplateFactory.getTemplate(env);
-        File ymlFile = dataLoader.loadYMlFile();
-        Yaml yaml = new Yaml();
         Tenant tenantTemp = null;
         try {
-            File resourceFile = dataLoader.loadCacheFile(env);
-            tenantTemp = objectMapper.readValue(new FileInputStream(resourceFile), Tenant.class);
+            tenantTemp = dataLoader.readDataFromCacheFile(env);
         } catch (Exception e) {
             logger.error("Cannot get the details");
         }
-        Map<String, Map<String, ArrayList<String>>> yamlMap;
-        try {
-            yamlMap = yaml.load(new FileInputStream(ymlFile));
-            if (yamlMap.size() != getAllCollections(env).size()) {
-                logger.error("The collection size mismatch found!, {} collections found in configuration file and {} collections found in the mongodb", yamlMap.size(), getAllCollections(env).size());
-            }
-            Tenant finalTenantTemp = tenantTemp;
-            yamlMap.entrySet().stream().parallel().forEach(entry -> {
-                String collectionName = entry.getKey();
-                String tenantPath = "";
-                String propertyPath = "";
-                if (entry.getValue().get("tenantId") != null) {
-                    tenantPath = entry.getValue().get("tenantId").stream().reduce((s1, s2) -> s1 + "." + s2).orElse("");
-                }
-                if (entry.getValue().get("propertyId") != null) {
-                    propertyPath = entry.getValue().get("propertyId").stream().reduce((s1, s2) -> s1 + "." + s2).orElse("");
-                }
-                Criteria criteria = null;
-                if (entry.getKey().equalsIgnoreCase("config") || entry.getKey().equalsIgnoreCase("configEvents")) {
-                    assert finalTenantTemp != null;
-                    Set<String> tenantAndProperty = finalTenantTemp.getProperty();
-                    tenantAndProperty.addAll(finalTenantTemp.getTenant());
-                    if (tenantAndProperty.size() == 0) {
-                        logger.info("No doucuments found for the " + collectionName);
-                        return;
-                    }
-                    String regex = tenantAndProperty.stream().collect(Collectors.joining("|"));
-                    criteria = Criteria.where("path").regex(regex);
-
-                } else if (!tenantPath.equalsIgnoreCase("") && !propertyPath.equalsIgnoreCase("")) {
-                    criteria = new Criteria().orOperator(
-                            Criteria.where(tenantPath).in(finalTenantTemp.getTenant()),
-                            Criteria.where(propertyPath).in(finalTenantTemp.getProperty())
-                    );
-                } else if (tenantPath.equalsIgnoreCase("") && !propertyPath.equalsIgnoreCase("")) {
-                    criteria = new Criteria().orOperator(
-
-                            Criteria.where(propertyPath).in(finalTenantTemp.getProperty())
-                    );
-                } else {
-                    criteria = new Criteria().orOperator(
-                            Criteria.where(tenantPath).in(finalTenantTemp.getTenant()));
-                }
-
-                Query query = new Query(criteria);
-                long count = mongoTemplate.count(query, collectionName);
-                logger.info(String.format("%s documents present in the %s collection from the cache, in the %s environment", count, collectionName, env));
-                documentCount.put(collectionName, (int) count);
-            });
-            return documentCount.toString();
-
-        } catch (FileNotFoundException e) {
-            return "Cannot make operation";
+        if (yamlMap.size() != getAllCollections(env).size()) {
+            logger.error("The collection size mismatch found!, {} collections found in configuration file and {} collections found in the mongodb", yamlMap.size(), getAllCollections(env).size());
         }
+        Tenant finalTenantTemp = tenantTemp;
+        yamlMap.entrySet().stream().parallel().forEach(entry -> {
+            String collectionName = entry.getKey();
+            String tenantPath = "";
+            String propertyPath = "";
+            if (entry.getValue().get("tenantId") != null) {
+                tenantPath = entry.getValue().get("tenantId").stream().reduce((s1, s2) -> s1 + "." + s2).orElse("");
+            }
+            if (entry.getValue().get("propertyId") != null) {
+                propertyPath = entry.getValue().get("propertyId").stream().reduce((s1, s2) -> s1 + "." + s2).orElse("");
+            }
+            Criteria criteria = null;
+            if (entry.getKey().equalsIgnoreCase("config") || entry.getKey().equalsIgnoreCase("configEvents")) {
+                assert finalTenantTemp != null;
+                Set<String> tenantAndProperty = finalTenantTemp.getProperty();
+                tenantAndProperty.addAll(finalTenantTemp.getTenant());
+                if (tenantAndProperty.size() == 0) {
+                    logger.info("No doucuments found for the " + collectionName);
+                    return;
+                }
+                String regex = tenantAndProperty.stream().collect(Collectors.joining("|"));
+                criteria = Criteria.where("path").regex(regex);
+
+            } else if (!tenantPath.equalsIgnoreCase("") && !propertyPath.equalsIgnoreCase("")) {
+                criteria = new Criteria().orOperator(
+                        Criteria.where(tenantPath).in(finalTenantTemp.getTenant()),
+                        Criteria.where(propertyPath).in(finalTenantTemp.getProperty())
+                );
+            } else if (tenantPath.equalsIgnoreCase("") && !propertyPath.equalsIgnoreCase("")) {
+                criteria = new Criteria().orOperator(
+
+                        Criteria.where(propertyPath).in(finalTenantTemp.getProperty())
+                );
+            } else {
+                criteria = new Criteria().orOperator(
+                        Criteria.where(tenantPath).in(finalTenantTemp.getTenant()));
+            }
+
+            Query query = new Query(criteria);
+            long count = mongoTemplate.count(query, collectionName);
+            logger.info(String.format("%s documents present in the %s collection from the cache, in the %s environment", count, collectionName, env));
+            documentCount.put(collectionName, (int) count);
+        });
+        return documentCount.toString();
+
     }
 
     public String getDocumentCountFromCacheDetailsAndBackup(String env) {
@@ -251,11 +243,11 @@ public class StayDeleteService {
         }
         Map<String, Integer> documentCount = new HashMap<>();
         MongoTemplate mongoTemplate = mongoTemplateFactory.getTemplate(env);
-        File ymlFile =dataLoader.loadCacheFile(env);
+        File ymlFile = dataLoader.loadCacheFile(env);
         Yaml yaml = new Yaml();
         Tenant tenantTemp = null;
         try {
-            File resourceFile =dataLoader.loadCacheFile(env);
+            File resourceFile = dataLoader.loadCacheFile(env);
             tenantTemp = objectMapper.readValue(new FileInputStream(resourceFile), Tenant.class);
         } catch (Exception e) {
             logger.error("Cannot get the details");
@@ -328,10 +320,8 @@ public class StayDeleteService {
     }
 
     public String getDataFromCache(String env) {
-        File resourceFile = Paths.get(System.getProperty("user.dir"), "src", "main", "resources", env + ".json").toFile();
-
         try {
-            Tenant temp = objectMapper.readValue(new FileInputStream(resourceFile), Tenant.class);
+            Tenant temp = dataLoader.readDataFromCacheFile(env);
             logger.info(String.format("Data returned from the cache for the %s environment is %s ", env, temp.toString()));
             return temp.toString();
         } catch (IOException e) {
