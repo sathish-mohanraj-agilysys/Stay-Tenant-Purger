53ff368623fc763fe6b5e3093cd9dec77d7f5de5
Sathish <sathish.m@agilysys.com>
2024-06-21 12:30:43 +0530

    interfaces updated

--- src/main/java/com/agilysys/StayTenantPurger/Interfaces/StayDeleteInterface.java
+++ src/main/java/com/agilysys/StayTenantPurger/Interfaces/StayDeleteInterface.java
@@ -12,25 +12,30 @@ import java.util.Map;
 import java.util.Set;
 
 public interface StayDeleteInterface {
-    @PostMapping("/delete/environment/{environment}")
+    @PostMapping("/addCache/environment/{environment}")
     String addTenantInCache(@RequestBody Tenant tenant, @PathVariable("environment") String env);
 
     @PostMapping("/delete/environment/{environment}/start")
     ResponseEntity startDeleting(@PathVariable("environment") String env, @RequestParam(value = "toDeleteCore",required = false) boolean isToDeleteCore);
 
-    @PostMapping("/delete/environment/{environment}/clear")
+    @PostMapping("/clearCache/environment/{environment}/clear")
     String clearDataInLocal(@PathVariable("environment") String env);
 
-    @GetMapping("/delete/environment/{environment}")
+    @GetMapping("/getCache/environment/{environment}")
     String getDataFromCache(@PathVariable("environment") String env);
-    @GetMapping("/delete/environment/{environment}/documentCount")
+
+    @GetMapping("/documentCount/environment/{environment}")
     String getDocumentCountFromCache(@PathVariable("environment") String env);
-    @PostMapping("/delete/environment/{environment}/dropCollection")
+
+    @PostMapping("/dropCollections/environment/{environment}")
     String dropCollections(@PathVariable("environment") String env);
-    @GetMapping("/environment/{environment}/getAllCollections")
+
+    @GetMapping("/allCollections/{environment}")
     Set<String> getAllCollections(@PathVariable("environment") String env);
-    @GetMapping("/environment/{environment}/getDocumentCount")
+
+    @GetMapping("/allDocumentCounts/{environment}")
     Map<String, Integer> getDocumentCount(@PathVariable("environment") String env);
+
     @PostMapping("/backup/environment/{environment}")
     String backupCollection(@PathVariable("environment") String env);
 }
