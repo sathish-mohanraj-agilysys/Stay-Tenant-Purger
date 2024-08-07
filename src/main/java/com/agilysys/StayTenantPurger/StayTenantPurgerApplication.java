dcdb0fbc159736ac67b516d0f9b66eb8667176e7
Sathish <sathish.m@agilysys.com>
2024-08-07 20:12:40 +0530

    mongo auto configuration has been reverted

--- src/main/java/com/agilysys/StayTenantPurger/StayTenantPurgerApplication.java
+++ src/main/java/com/agilysys/StayTenantPurger/StayTenantPurgerApplication.java
@@ -3,14 +3,15 @@ package com.agilysys.StayTenantPurger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
 @EnableScheduling
 public class StayTenantPurgerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StayTenantPurgerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StayTenantPurgerApplication.class, args);
+    }
 
 }
