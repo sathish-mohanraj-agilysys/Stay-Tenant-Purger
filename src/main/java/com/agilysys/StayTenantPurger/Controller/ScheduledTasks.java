f8f936f711b99014ab4d3706dfe3daa26517f97d
Sathish <sathish.m@agilysys.com>
2024-08-14 11:46:52 +0530

    scheduled task is added with the new cron setting

--- src/main/java/com/agilysys/StayTenantPurger/Controller/ScheduledTasks.java
+++ src/main/java/com/agilysys/StayTenantPurger/Controller/ScheduledTasks.java
@@ -1,27 +1,41 @@
-package com.agilysys.StayTenantPurger.Config;
+package com.agilysys.StayTenantPurger.Controller;
 
-import com.agilysys.StayTenantPurger.Service.StayMongoDeleteService;
+import com.agilysys.StayTenantPurger.Service.StaleChecker;
+import com.agilysys.StayTenantPurger.modal.DAO.Tenant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
+import java.util.Set;
 
 @Component
 public class ScheduledTasks {
     @Autowired
-    private StayMongoDeleteService stayMongoDeleteService;
+    private MainController mainController;
+    @Autowired
+    private StaleChecker staleChecker;
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-    private String[] env = new String[]{"000", "005"};
+    private String[] env = new String[]{"005"};
 
-    @Scheduled(cron = "0 0 3 * * ?")
+    @PostConstruct
+    private void init() {
+        logger.info("[{]] Automation environment cleanup");
+        weeklyTaskinitiated();
+    }
+
+    @Scheduled(cron = "0 */5 * * * ?")
     public void weeklyTaskinitiated() {
         logger.info("Daily Task initiated");
-        Arrays.stream(env).forEach(x -> {
-            stayMongoDeleteService.deleteInMongodb(x, true);
-            logger.info("The {} environment cleaned up",x);
+        Arrays.stream(env).forEach(env -> {
+            Tenant tenant = new Tenant();
+//            tenant.setTenant(staleChecker.checkTenants(env, false));
+//            tenant.setProperty(Set.of());
+//            mainController.startDeletingSync(tenant, env);
+            logger.info("The {} environment cleaned up", env);
         });
     }
 }
